0ef2e08f1d4d67eb86d3814c6b6c3224371de76e
priusf0x <priusfoxlittle@gmail.com>
2025-11-05 21:28:59 +0300

    Minor Doxy Update

--- src/list/list.h
+++ src/list/list.h
@@ -46,7 +46,7 @@ list_return_e DestroyList(list_t** list);
 // =================== ELEMENT_CONTROL ====================
 /**
  * \brief Function for adding elements to list
- * Adds the element in the nearest one free position in list.
+ * \details Adds the element in the nearest one free position in list.
  * \param list *list_t** list where the element will be added
  * \param[in] value *data_type* value of the element that should be added
  * \return *list_return_e* returns the result of adding
@@ -54,7 +54,7 @@ list_return_e DestroyList(list_t** list);
 list_return_e ListAddElement(list_t* list, data_type value);
 /**
  * \brief Function for adding elements to list
- * Adds the element after defined index in list.
+ * \details Adds the element after defined index in list.
  * \param list *list_t** list where the element will be added
  * \param[in] value *data_type* value of the element that should be added
  * \param index *ssize_t* the index after which element will be placed
@@ -63,7 +63,7 @@ list_return_e ListAddElement(list_t* list, data_type value);
 list_return_e ListAddAfterElement(list_t* list, data_type value, size_t index);
 /**
  * \brief Function for deleting elements from list
- * Deletes the element on *index* position
+ * \details Deletes the element on *index* position
  * \param list *list_t** list where the element will be deleted
  * \param[in] value *data_type* value of the element that should be added
  * \return *list_return_e* returns the result of deleting
@@ -74,7 +74,7 @@ list_return_e ListDeleteElement(list_t* list, size_t  index);
 
 /**
  * \brief Function for getting indo
- * Function for getting element value.
+ * \details Function for getting element value.
  * \param list *list_t**
  * \param element_index *size_t* index of outcome value
  * \param[out] value *data_type** pointer for returning a value
@@ -83,7 +83,7 @@ list_return_e ListDeleteElement(list_t* list, size_t  index);
 list_return_e GetElementValue(list_t* list, size_t element_index, data_type* value);
 /**
  * \brief Navigation function
- * Function for getting index of next element.
+ * \details Function for getting index of next element.
  * \param list *list_t**
  * \param element_index *size_t*
  * \return *ssize_t* index of the next element
@@ -91,7 +91,7 @@ list_return_e GetElementValue(list_t* list, size_t element_index, data_type* val
 ssize_t GetNextElement(list_t* list, size_t element_index);
 /**
  * \brief Navigation function
- * Function for getting index of previous element.
+ * \details Function for getting index of previous element.
  * \param list *list_t**
  * \param element_index *size_t*
  * \return *ssize_t* index of the previous element
@@ -99,14 +99,14 @@ ssize_t GetNextElement(list_t* list, size_t element_index);
 ssize_t GetPreviousElement(list_t* list, size_t element_index);
 /**
  * \brief Navigation function
- * Function for getting index of the first list element.
+ * \details Function for getting index of the first list element.
  * \param list *list_t**
  * \return *ssize_t* index of the first element
  */
 ssize_t GetHeadElement(list_t* list);
 /**
  * \brief Navigation function
- * Function for getting index of the last list element.
+ * \details Function for getting index of the last list element.
  * \param list *list_t**
  * \return *ssize_t* index of the last element
  */
@@ -115,7 +115,7 @@ ssize_t GetTailElement(list_t* list);
 // ====================== LIST_LOG ========================
 /**
  * \brief Navigation function
- * Function for getting index of the last list element.
+ * \details Function for getting index of the last list element.
  * \param comment *const char** comment for adding in dump.
  * \param list *list_t**
  */
@@ -132,12 +132,12 @@ list_return_e ListVerify(list_t* list);
 /**
  * If defined *NDEBUG* no verification during calculatuons will be done
  */
+
 #ifndef NDEBUG
-#define VERIFY_RET(X) if (ListVerify(X) != 0) {return ListVerify(X);}
+#define VERIFY_RET(X) do{ if (ListVerify(X) != 0) {return ListVerify(X);}}while(0)
 #else
 #define VERIFY_RET(X)
 #endif
-
 
 //=========================================================
 
